f6c2881d0667528b4318f9c07e47a535bb5acced
update-awesome <4371805@qq.com>
2022-04-17 15:20:11 +0800

    add .env support

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
 const axios = require('axios')
 const sleep = require('sleep')
-const cliProgress = require('cli-progress');
+const cliProgress = require('cli-progress')
 
-const GITHUB_USERNAME = 'romejiang'
-const GITHUB_TOKEN = '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'
+require('dotenv').config()
+
 const README_FILENAME = '/README.md'
-const TARGET_FILENAME = '/awesome-vue-with-stars.md'
+const TARGET_FILENAME = '/awesome-with-stars.md'
 const DEBUG = false
+// const DEBUG = true
+let DATEBASE_NAME = 'db'
 
 const getStar = async (repo) => {
   if (DEBUG) {
@@ -18,21 +20,26 @@ const getStar = async (repo) => {
   })
   if (star && star.star) {
     return star.star
-  } else if(repo.indexOf('undefined') > -1){
+  } else if (repo.indexOf('undefined') > -1) {
     return 0
-  } else if(repo.split('/').length != 2){
+  } else if (repo.split('/').length != 2) {
     return 0
-  }else{
+  } else {
     sleep.msleep(700)
+    DEBUG && console.log('get star: ' + repo)
     const response = await axios.get('https://api.github.com/repos/' + repo, {
       auth: {
-        username: GITHUB_USERNAME,
-        password: GITHUB_TOKEN,
+        username: process.env.GITHUB_USERNAME,
+        password: process.env.GITHUB_TOKEN,
+      },
+      header: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
       },
     })
     saveDatabase({
       id: repo,
-      star: response.data.stargazers_count
+      star: response.data.stargazers_count,
     })
     return response.data.stargazers_count
   }
@@ -43,20 +50,21 @@ function initDatabase() {
 }
 
 function findDatabase(data) {
-  const dbfile = './db.json'
+  const dbfile = `./${DATEBASE_NAME}.json`
+  DEBUG && console.log('find database: ' + dbfile)
   let database = []
   if (fs.existsSync(dbfile)) {
-    database = require('./db')
+    database = require('./' + DATEBASE_NAME)
   }
   let index = database.findIndex((e) => e.id === data.id)
   return index === -1 ? null : database[index]
 }
 
 function saveDatabase(data) {
-  const dbfile = './db.json'
+  const dbfile = `./${DATEBASE_NAME}.json`
   let database = []
   if (fs.existsSync(dbfile)) {
-    database = require('./db')
+    database = require('./' + DATEBASE_NAME)
   }
   let index = database.findIndex((e) => e.id === data.id)
   let obj
@@ -72,51 +80,51 @@ function saveDatabase(data) {
 }
 
 const proc = async (text) => {
-  const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-
-
-  const reg = /\[[^\n]*?\]\(([^\n]*?)\)/gim
   let total = 0
   let progres = 0
+
+  const reg = /\[[^\n]*?\]\(([^\n]*?)\)/gim
   while ((itme = reg.exec(text))) {
     total++
   }
-  bar1.start(total, 0);
-  // console.log("total:", total) 
+  DEBUG && console.log('total: ' + total)
+
+  const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+  progressBar.start(total, 0)
+
   const texts = []
   const indexs = []
   while ((itme = reg.exec(text))) {
     const link = itme[1]
-    progres++
-    bar1.update(progres);
+
+    progressBar.update(progres++)
     if (link.startsWith('https://github.com')) {
       const repo = link.split('/')[3] + '/' + link.split('/')[4]
-      if(link.split('/')[4] == 'undefined'){
-        continue;
-      }
+
       try {
         const star = await getStar(repo)
-        const length = ('' + star).length
-        const blank = '&nbsp;'.repeat(6 - length)
-        const text = `<code>${blank}${star}</code> ![](https://img.shields.io/github/last-commit/${repo}) `
+        if (star > 0) {
+          const length = ('' + star).length
+          const blank = '&nbsp;'.repeat(6 - length)
+          const text = `<code>${blank}${star}</code> ![](https://img.shields.io/github/last-commit/${repo}) `
 
-        texts.push(text)
-        indexs.push(itme.index)
+          texts.push(text)
+          indexs.push(itme.index)
+        }
         // console.log(repo, star)
       } catch (e) {
-        console.log(e.toString())
-        console.log(repo)
+        DEBUG && console.log(e)
+        console.log('error: ' + repo)
       }
     }
   }
-  console.log("total:", total)
   let nText = text
   for (let index = indexs.length - 1; index >= 0; index--) {
     const idx = indexs[index]
     const text = texts[index]
     nText = nText.slice(0, idx) + text + nText.slice(idx)
   }
-  bar1.stop();
+  progressBar.stop()
   return nText
 }
 
@@ -125,6 +133,8 @@ const proc = async (text) => {
   if (arguments.length > 1) {
     initDatabase()
   }
+  DATEBASE_NAME = arguments[0].replace(/[\/|\.]*/g, '')
+
   const data = fs.readFileSync(arguments[0] + README_FILENAME, 'utf8')
 
   const content = await proc(data)
